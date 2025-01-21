a068947495de2620babc015b9b37a79bd3ce4434
Jaunty <jaunty5899@gmail.com>
2025-01-21 17:15:53 +0530

    Tasks block done

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -1,5 +1,12 @@
 import "./Tasks.css";
 
 export default function Tasks() {
-  console.log("hello");
+  return (
+    <div className="tasksContainer">
+      <div className="taskContainer">
+        <input type="checkbox" id="task" />
+        <label htmlFor="task">New task is created and added to the list</label>
+      </div>
+    </div>
+  );
 }
